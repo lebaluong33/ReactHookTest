23ef9fce586f5951193fe52f86975ee8ff37c6b6
lebaluong33@gmail.com <lebaluong33@gmail.com>
2020-02-25 21:55:54 +0700

    initiate commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import Todo from './components/todo';
+import Header from './components/Header';
+import Auth from './components/Auth';
+import AuthContext from './auth-context';
+
+const App = props => {
+  return (
+    <div className="App">
+      <h1>WellCome</h1>
+    </div>
+  );
+}
+
+export default App;
